dc12b190f01a7d2fc45c10e402c9b8d344214d4f
cedie712 <cedrick048@yahoo.com>
2019-01-26 23:17:58 +0800

    revise initial UIs

---
+++ CCTTI_Web_App/cctti_webapp/static/cctti_webapp/js/courses.js
@@ -0,0 +1,74 @@
+ScrollReveal().reveal('.title-wrapper',
+    {
+        origin: 'left',
+        delay: 200,
+        distance : '100px',
+        easing: 'ease-in-out',
+        // scale: 0.40
+    });
+
+
+ScrollReveal().reveal('.local-guiding-service',
+    {
+        // origin: 'left',
+        delay: 300,
+        distance : '270px',
+        easing: 'ease-in-out',
+        // scale: 0.20
+    });
+
+ScrollReveal().reveal('.computer-systems',
+    {
+        // origin: 'left',
+        delay: 200,
+        distance : '420px',
+        easing: 'ease-in-out',
+        // scale: 0.50
+    });
+
+ScrollReveal().reveal('.events-management',
+    {
+        // origin: 'left',
+        delay: 400,
+        distance : '300px',
+        easing: 'ease-in-out',
+        // scale: 0.40
+    });
+
+//////
+
+ScrollReveal().reveal('.bookkeeping',
+    {
+        // origin: 'left',
+        delay: 200,
+        distance : '220px',
+        easing: 'ease-in-out',
+        // scale: 0.20
+    });
+
+ScrollReveal().reveal('.electronics',
+    {
+        // origin: 'left',
+        delay: 400,
+        distance : '220px',
+        easing: 'ease-in-out',
+        // scale: 0.50
+    });
+
+ScrollReveal().reveal('.tourism-promotion',
+    {
+        // origin: 'left',
+        delay: 100,
+        distance : '220px',
+        easing: 'ease-in-out',
+        // scale: 0.40
+    });
+
+ScrollReveal().reveal('.btn-container',
+    {
+        // origin: 'left',
+        delay: 350,
+        distance : '120px',
+        easing: 'ease-in-out',
+        scale: 0.20
+    });
