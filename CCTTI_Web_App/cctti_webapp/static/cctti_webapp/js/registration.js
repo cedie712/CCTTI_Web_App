aa182da95d8b3aa7c55f4b44b46ecdd17f0a13f6
cedie712 <cedrick048@yahoo.com>
2019-01-28 22:58:34 +0800

    prepare registration slider

--- CCTTI_Web_App/cctti_webapp/static/cctti_webapp/js/registration.js
+++ CCTTI_Web_App/cctti_webapp/static/cctti_webapp/js/registration.js
@@ -1,44 +1,30 @@
-// just querying the DOM...like a boss!
-var links = document.querySelectorAll(".itemLinks");
-var wrapper = document.querySelector(".slide-container");
+$('.carousel-control-prev').hide();
 
-// the activeLink provides a pointer to the currently displayed item
-var activeLink = 0;
+$(".carousel").carousel('pause');
 
-// setup the event listeners
-for (var i = 0; i < links.length; i++) {
-    var link = links[i];
-    link.addEventListener('click', setClickedItem, false);
+$(".carousel-control-next").click(function(){
+  $(".carousel").carousel("next");
+});
 
-    // identify the item for the activeLink
-    link.itemID = i;
-}
+$(".carousel-control-prev").click(function(){
+  $(".carousel").carousel("prev");
+});
 
-// set first item as active
-links[activeLink].classList.add("active");
 
-function setClickedItem(e) {
-    removeActiveLinks();
-
-    var clickedLink = e.target;
-    activeLink = clickedLink.itemID;
-
-    changePosition(clickedLink);
-}
-
-function removeActiveLinks() {
-    for (var i = 0; i < links.length; i++) {
-        links[i].classList.remove("active");
-    }
-}
-
-// Handle changing the slider position as well as ensure
-// the correct link is highlighted as being active
-function changePosition(link) {
-    var position = link.getAttribute("data-pos");
-
-    var translateValue = "translate3d(" + position + ", 0px, 0)";
-    wrapper.style.transform = translateValue;
-
-    link.classList.add("active");
-}
+$('.carousel').carousel({
+  wrap: false
+}).on('slid.bs.carousel', function () {
+    curSlide = $('.active');
+  if(curSlide.is( ':first-child' )) {
+     $('.carousel-control-prev').hide();
+     return;
+  } else {
+     $('.carousel-control-prev').show();
+  }
+  if (curSlide.is( ':last-child' )) {
+     $('.carousel-control-next').hide();
+     return;
+  } else {
+     $('.carousel-control-next').show();
+  }
+});
