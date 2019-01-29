0fcdc2a8f77f7aae64bc4a15ab449f88c56e529d
cedie712 <cedrick048@gmail.com>
2019-01-29 16:09:15 +0800

    finish registration view

--- CCTTI_Web_App/cctti_webapp/static/cctti_webapp/js/registration.js
+++ CCTTI_Web_App/cctti_webapp/static/cctti_webapp/js/registration.js
@@ -1,30 +1,63 @@
-$('.carousel-control-prev').hide();
+if (window.innerWidth > 700) {
+    $('.carousel-control-prev').hide();
 
-$(".carousel").carousel('pause');
+    $(".carousel-control-next").click(function(){
+      $(".carousel").carousel("next");
+    });
 
-$(".carousel-control-next").click(function(){
-  $(".carousel").carousel("next");
-});
-
-$(".carousel-control-prev").click(function(){
-  $(".carousel").carousel("prev");
-});
+    $(".carousel-control-prev").click(function(){
+      $(".carousel").carousel("prev");
+    });
 
 
-$('.carousel').carousel({
-  wrap: false
-}).on('slid.bs.carousel', function () {
-    curSlide = $('.active');
-  if(curSlide.is( ':first-child' )) {
-     $('.carousel-control-prev').hide();
-     return;
-  } else {
-     $('.carousel-control-prev').show();
-  }
-  if (curSlide.is( ':last-child' )) {
-     $('.carousel-control-next').hide();
-     return;
-  } else {
-     $('.carousel-control-next').show();
-  }
-});
+    $('.carousel').carousel({
+      wrap: false
+    }).on('slid.bs.carousel', function () {
+        curSlide = $('.active');
+      if(curSlide.is( ':first-child' )) {
+         $('.carousel-control-prev').hide();
+         return;
+      } else {
+         $('.carousel-control-prev').show();
+      }
+      if (curSlide.is( ':last-child' )) {
+         $('.carousel-control-next').hide();
+         return;
+      } else {
+         $('.carousel-control-next').show();
+      }
+    });
+}
+else {
+
+    $('.button-prev').hide();
+
+    $(".button-next").click(function(){
+      $(".carousel").carousel("next");
+    });
+
+    $(".button-prev").click(function(){
+      $(".carousel").carousel("prev");
+    });
+
+
+    $('.carousel').carousel({
+      wrap: false
+    }).on('slid.bs.carousel', function () {
+        curSlide = $('.active');
+      if(curSlide.is( ':first-child' )) {
+         $('.button-prev').hide();
+         return;
+      } else {
+         $('.button-prev').show();
+      }
+      if (curSlide.is( ':last-child' )) {
+         $('.button-next').hide();
+         return;
+      } else {
+         $('.button-next').show();
+      }
+    });
+}
+
+
